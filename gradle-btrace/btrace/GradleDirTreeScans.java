d241ecbad46c304a3122821351f2bb971879b83d
Lari Hotari <lari@hotari.net>
2015-10-05 16:58:51 -0400

    add DirectoryFileTree profiler

--- gradle-btrace/btrace/GradleDirTreeScans.java
+++ gradle-btrace/btrace/GradleDirTreeScans.java
@@ -3,40 +3,36 @@ import com.sun.btrace.Profiler;
 import com.sun.btrace.annotations.*;
 
 import java.util.Deque;
+import java.io.File;
 
 import static com.sun.btrace.BTraceUtils.*;
 
 @BTrace
-public class GradleTaskInputsAndOutputs {
-    @Property(name = "GradleTaskInputsAndOutputs")
+public class GradleDirTreeScans {
+    @Property(name = "GradleDirTreeScans")
     public static Profiler profiler = Profiling.newProfiler();
 
     @TLS
     public static Deque<Long> entryTimes = Collections.newDeque();
 
     @OnMethod(
-            clazz = "/org\\.gradle\\.api\\.internal\\.tasks\\.DefaultTask(Out|In)puts/",
-            method = "getFiles"
+            clazz = "org.gradle.api.internal.file.collections.DirectoryFileTree",
+            method = "visit"
     )
     public static void entry() {
         Collections.push(entryTimes, box(timeNanos()));
     }
 
     @OnMethod(
-            clazz = "/org\\.gradle\\.api\\.internal\\.tasks\\.DefaultTask(Out|In)puts/",
-            method = "getFiles",
+            clazz = "org.gradle.api.internal.file.collections.DirectoryFileTree",
+            method = "visit",
             location = @Location(Kind.RETURN)
     )
-    public static void exit(@Self Object thisObject, @Return Object returnValue) {
+    public static void exit(@Self Object thisObject) {
         long startTime = unbox(Collections.removeFirst(entryTimes));
         long duration = timeNanos() - startTime;
-        String displayName;
-        if(compare(classOf(thisObject), classForName("org.gradle.api.internal.tasks.DefaultTaskInputs", contextClassLoader()))) {
-            Object inputFiles = Reflective.get(Reflective.field(classOf(thisObject), "inputFiles"), thisObject);
-            displayName = (String) Reflective.get(Reflective.field(classOf(inputFiles), "displayName"), inputFiles);
-        } else {
-            displayName = (String) Reflective.get(Reflective.field(classOf(returnValue), "displayName"), returnValue);
-        }
+        File dir = (File)Reflective.get(Reflective.field(classOf(thisObject), "dir"), thisObject);
+        String displayName = Strings.str(dir);
         Profiling.recordEntry(profiler, displayName);
         Profiling.recordExit(profiler, displayName, duration);
     }
