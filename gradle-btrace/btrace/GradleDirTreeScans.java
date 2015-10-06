923bc49f1dfc8510dd4bfa7c98cac1142b324e4d
Lari Hotari <lari@hotari.net>
2015-10-05 21:14:21 -0400

    print and reset counters at the end of the build

--- gradle-btrace/btrace/GradleDirTreeScans.java
+++ gradle-btrace/btrace/GradleDirTreeScans.java
@@ -1,9 +1,11 @@
+import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.BTraceUtils.*;
 import com.sun.btrace.Profiler;
 import com.sun.btrace.annotations.*;
+import com.sun.btrace.annotations.BTrace;
 
-import java.util.Deque;
 import java.io.File;
+import java.util.Deque;
 
 import static com.sun.btrace.BTraceUtils.*;
 
@@ -37,13 +39,13 @@ public class GradleDirTreeScans {
         Profiling.recordExit(profiler, displayName, duration);
     }
 
-    @OnTimer(5000)
-    public static void timer() {
-        Profiling.printSnapshot("GradleTaskInputsAndOutputs", profiler, "%1$s %2$s %8$s %9$s %10$s");
-    }
-
-    @OnEvent("clear_stats")
+    @OnMethod(clazz = "org.gradle.launcher.exec.InProcessBuildActionExecuter$DefaultBuildController",
+            method = "run",
+            location = @Location(Kind.RETURN))
     public static void reset() {
+        BTraceUtils.println("--------------------------------------------------");
+        Profiling.printSnapshot("GradleDirTreeScans", profiler, "%1$s %2$s %8$s %9$s %10$s");
+        BTraceUtils.println("--------------------------------------------------");
         Profiling.reset(profiler);
     }
 }
--- gradle-btrace/btrace/GradleTaskInputsAndOutputs.java
+++ gradle-btrace/btrace/GradleTaskInputsAndOutputs.java
@@ -1,3 +1,4 @@
+import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.BTraceUtils.*;
 import com.sun.btrace.Profiler;
 import com.sun.btrace.annotations.*;
@@ -41,13 +42,13 @@ public class GradleTaskInputsAndOutputs {
         Profiling.recordExit(profiler, displayName, duration);
     }
 
-    @OnTimer(5000)
-    public static void timer() {
-        Profiling.printSnapshot("GradleTaskInputsAndOutputs", profiler, "%1$s %2$s %8$s %9$s %10$s");
-    }
-
-    @OnEvent("clear_stats")
+    @OnMethod(clazz = "org.gradle.launcher.exec.InProcessBuildActionExecuter$DefaultBuildController",
+            method = "run",
+            location = @Location(Kind.RETURN))
     public static void reset() {
+        BTraceUtils.println("--------------------------------------------------");
+        Profiling.printSnapshot("GradleTaskInputsAndOutputs", profiler, "%1$s %2$s %8$s %9$s %10$s");
+        BTraceUtils.println("--------------------------------------------------");
         Profiling.reset(profiler);
     }
 }
